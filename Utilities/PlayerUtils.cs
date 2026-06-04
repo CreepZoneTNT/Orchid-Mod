86f51107d07df62399e17a780fbc72195fbc8b85
Jake Hatton <jakehatton6@gmail.com>
2026-06-04 17:51:14 -0400

    OrchidPlayer shortcut in PlayerUtils.cs

---
+++ Utilities/PlayerUtils.cs
@@ -0,0 +1,15 @@
+using OrchidMod.Common.ModObjects;
+using Terraria;
+using OrchidMod.Content.Guardian;
+using OrchidMod.Content.Shapeshifter;
+
+namespace OrchidMod.Utilities;
+
+public static partial class OrchidUtils
+{
+	public static OrchidPlayer OrchidPlayer(this Player player) => player.GetModPlayer<OrchidPlayer>();
+	public static OrchidGuardian Guardian(this Player player) => player.GetModPlayer<OrchidGuardian>();
+	public static OrchidShapeshifter Shapeshifter(this Player player) => player.GetModPlayer<OrchidShapeshifter>();
+	public static OrchidAlchemist Alchemist(this Player player) => player.GetModPlayer<OrchidAlchemist>();
+	public static OrchidGambler Gambler(this Player player) => player.GetModPlayer<OrchidGambler>();
+}
